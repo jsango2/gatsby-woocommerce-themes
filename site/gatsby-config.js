f0d9249dd61aa78d6638a2e59e629c2961a5384d
jsango2 <jure.sango@gmail.com>
2021-02-22 23:00:01 +0100

    first commit

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -4,12 +4,13 @@ module.exports = {
 	plugins: [
 		// Tell gatsby which theme you will be using and other configs.
 		{
-		resolve: "gatsby-woocommerce-theme",
-		options: {
-			wordPressUrl: `${ process.env.WORDPRESS_SITE_URL }`,
-			gatsbySiteUrl: `${ process.env.GATSBY_SITE_URL }`,
-			googleTagManagerId: `${ process.env.GOOGLE_TAGMANAGER_ID }`,
-			fbAppId: `${ process.env.FB_APP_ID }`
-		}
-	} ]
+			resolve: 'gatsby-woocommerce-theme',
+			options: {
+				wordPressUrl: 'https://www.sutrashop.com.hr',
+				gatsbySiteUrl: 'https://www.sutrashop.com.hr',
+				googleTagManagerId: `${process.env.GOOGLE_TAGMANAGER_ID}`,
+				fbAppId: `${process.env.FB_APP_ID}`,
+			},
+		},
+	],
 };
